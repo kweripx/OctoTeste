c6d127f485b831090bc0178e0140fd991b80b2ec
kweripx <vitor.hugoqueiroz11@gmail.com>
2021-05-18 16:11:43 -0300

    Adicionar arquivos de projeto.

---
+++ OctoTeste/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OctoTeste.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
